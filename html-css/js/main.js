b87aca3f55e461c81a4924dad7f74ebcf624add0
torikkyun <nduc42176@gmail.com>
2025-05-25 23:23:22 +0700

    Sửa lại header sidebar

--- html-css/js/main.js
+++ html-css/js/main.js
@@ -1,60 +1,64 @@
-function getPageTitle() {
-  const path = window.location.pathname;
-  switch (path) {
-    case "/pages/dashboard.html":
-      return "Trang chủ";
-    case "/pages/users.html":
-      return "Người dùng";
-    case "/pages/devices.html":
-      return "Thiết bị";
-    case "/pages/projects.html":
-      return "Dự án";
-    case "/pages/profile.html":
-      return "Hồ sơ";
-    case "/pages/loans.html":
-      return "Mượn trả thiết bị";
-    default:
-      return "Trang chủ";
-  }
-}
+// function getPageTitle() {
+//   const path = window.location.pathname;
+//   switch (path) {
+//     case "/pages/dashboard.html":
+//       return "Trang chủ";
+//     case "/pages/users.html":
+//       return "Người dùng";
+//     case "/pages/devices.html":
+//       return "Thiết bị";
+//     case "/pages/projects.html":
+//       return "Dự án";
+//     case "/pages/profile.html":
+//       return "Hồ sơ";
+//     case "/pages/loans.html":
+//       return "Mượn trả thiết bị";
+//     default:
+//       return "Trang chủ";
+//   }
+// }
 
-function setHeaderTitle() {
-  const headerTitle = document.querySelector(".header-title");
-  if (headerTitle) {
-    headerTitle.textContent = getPageTitle();
-  }
-}
+// function setHeaderTitle() {
+//   const headerTitle = document.querySelector(".header-title");
+//   if (headerTitle) {
+//     headerTitle.textContent = getPageTitle();
+//   }
+// }
 
-function handleLogout() {
-  localStorage.removeItem("access_token");
-  window.location.href = "/pages/signin.html";
-}
+// function handleLogout() {
+//   localStorage.removeItem("access_token");
+//   window.location.href = "/pages/signin.html";
+// }
 
 function loadLayout() {
   axios
-    .get("/partials/header.html")
+    .get("/components/header.html")
     .then((response) => {
-      document.getElementById("header-container").innerHTML = response.data;
-      setHeaderTitle();
+      document.getElementById("header").outerHTML = response.data;
+      // setHeaderTitle();
     })
     .catch((error) => console.error("Lỗi khi load header:", error));
 
   axios
-    .get("/partials/sidebar.html")
+    .get("/components/sidebar.html")
     .then((response) => {
-      document.getElementById("sidebar-container").innerHTML = response.data;
-      setActiveMenuItem();
-      initializeSidebarEvents();
-      initializeSidebarToggle();
+      document.getElementById("sidebar").outerHTML = response.data;
+
+      const toggle = document.querySelector(".btn-toggle");
+      const sidebar = document.querySelector("aside");
+      toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("show-sidebar");
+        sidebar.classList.toggle("hide-sidebar");
+      });
     })
     .catch((error) => console.error("Lỗi khi load sidebar:", error));
 
-  axios
-    .get("/partials/footer.html")
-    .then((response) => {
-      document.getElementById("footer-container").innerHTML = response.data;
-    })
-    .catch((error) => console.error("Lỗi khi load footer:", error));
+  // axios
+  //   .get("/components/footer.html")
+  //   .then((response) => {
+  //     document.getElementById("footer-container").innerHTML = response.data;
+  //   })
+  //   .catch((error) => console.error("Lỗi khi load footer:", error));
 }
 
 document.addEventListener("DOMContentLoaded", loadLayout);
--- html-css/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  server: {
-    port: 5500,
-  },
-});
